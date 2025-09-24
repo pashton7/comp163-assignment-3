db8e78022139f3697f7465fc0ed7f26353985369
Samicoo <pashton7167@gmail.com>
2025-09-24 10:35:20 -0400

    Pass Test Case 1: Initial game setup with required variables

---
+++ AshtonP_Assignment_3.py
@@ -0,0 +1,7 @@
+student_name = "Ashton"
+current_gpa = 3.7
+study_hours = 30
+social_points = 40
+stress_level = 20
+
+print(f"Hello {student_name}, your GPA is {current_gpa}, your study hours are {study_hours}, you have {social_points} social points, and your stress level is {stress_level}")
