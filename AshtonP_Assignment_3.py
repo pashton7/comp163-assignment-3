ecdb56dfc8c5986b68d02c613380dc46a2fb6c9f
Samicoo <pashton7167@gmail.com>
2025-09-26 11:45:56 -0400

    Fixed errors/game logic. Game loop now runs twice to have end game results differ.

--- AshtonP_Assignment_3.py
+++ AshtonP_Assignment_3.py
@@ -7,6 +7,7 @@ heavy_weapons = False
 has_scanner = False
 money = 0
 
+event_choices = []
 game_event = ["Salvage","Battle","Rescue"]
 
 # Have users pick a ship focus that will greatly affect the outcome/ending
@@ -29,12 +30,15 @@ else:
 print("Now that your ready to venture into space let's decide what you want to do.")
 print(f"Choose from the following options on what you want to do {game_event}: ")
 event_choice = input(f"Your choice {game_event}: ")
+event_choices.append(event_choice)
+print()
 
 # Handle the game logic and branching where each player choice effects the ending
 if event_choice == "Salvage":
+    game_event.remove(event_choice)
     if has_scanner == True:
         print("Your scanners picked up no dangerous lifeforms or dangerous ships, leading you to a successful salvage. You gained +25 Money")
-        credits += 25
+        money += 25
     elif ship_choice == "A":
         print("Because you have no scanners you are unaware of any danger. While attempting to salvage a rouge turret fires upon your ship. Ship Health: -10, Money: +15")
         ship_health -= 10
@@ -44,21 +48,22 @@ if event_choice == "Salvage":
         money += 6
    
 elif event_choice == "Rescue":
+    game_event.remove(event_choice)
     if has_scanner == True:
-        print("""After recieving a distress signal you scan the vessel for danger. Your scanners pick up that 
-              the distress signal is coming from a crew of dangerous rouge androids
-              you destroy their vessel and salvage the wreck. Money: +10
-              """)
+        print("After recieving a distress signal you scan the vessel for danger. Your scanners pick up that the distress signal is coming from a crew of dangerous rouge \nandroids you destroy their vessel and salvage the wreck. Money: +10")
+        money += 10
     elif ship_choice == "A":
         print("You've just recieved a distress signal, but you are unaware of any danger that might be present.")
-        print("You must choose your next decision wisely: A.) Take no chances and destroy the ship\nB.) Take the chance of saving lives")
+        print("You must choose your next decision wisely:\nA.) Take no chances and destroy the ship\nB.) Take the chance of saving lives")
         rescue_choice = input("Your choice (A, B): ")
         if rescue_choice == "A":
             print("Unwilling to take any risks you destroy their ship. Unfortunatly those aboard were infact in need of aid. But at least you were able to salvage something. Money: +20")
             money += 20
         elif rescue_choice == "B":
-            print("Being the selfless captain you are you take the risk and help those in need. Luckily for you the crew you saved were peaceful. In their gratitude they offer you money and one pledges himself to your crew.")
+            print("Being the selfless captain you are you take the risk and help those in need. Luckily for you the crew you saved were peaceful. In their gratitude they \noffer you money and one pledges himself to your crew.")
             print(f'Crew member gained: Rogers, Money: +10')
+            money += 10
+            ship_crew.append("Rogers")
         else:
             print(f"'{rescue_choice}' is not a valid option. Please try again.")
     elif heavy_weapons == True:
@@ -67,8 +72,9 @@ elif event_choice == "Rescue":
         print(f"Ship damaged: -30, Crew member lost: {ship_crew[1]}, Money: +25")
         ship_health -= 30
         ship_crew.pop(1)
-        money +=25
+        money += 25
 elif event_choice == "Battle":
+    game_event.remove(event_choice)
     if has_scanner:
         print("While flying across the galaxy you encounter a vessel with a bounty on its head. Unfortunatly they have jammed your scanners leaving you unaware of their power.")
         print("Will you decide to:\nA.) Engage in battle and collect the bounty\nB.) Do not engage the ship and flee")
@@ -96,16 +102,114 @@ elif event_choice == "Battle":
         else:
             print(f"{battle_choice2} is not a valid option. Please try again.")
     elif heavy_weapons == True:
-        print("You come across a battle between smugglers and a bounty hunter. They both have requested of your help, the smugglers offer you a portion of their goods but the bounty hunter you offers you a portion of the bounty reward.")
+        print("You come across a battle between smugglers and a bounty hunter. They both have requested of your help, the smugglers offer you a portion of their goods but the bounty hunter offers you a portion of the bounty reward.")
         print("What will you do?:\nA.) Side with the smugglers they might have valuable goods.\nB.) Side with the bounty hunter, the reward might be greater than the risks.\nC.) My enemies are many, my equals are none. Attack both and claim the rewards for yourself!")
         battle_choice3 = input("Your choice (A, B, C): ")
         if battle_choice3 == "A":
-            print("You decide to team up with the smugglers and you easily take down the bounty hunter. As thanks the smugglers give you a portion of their loot and you salvage the remains of the bounty hunters ship.")
+            print("You decide to team up with the smugglers and you easily take down the bounty hunter. As thanks the smugglers give you a portion of their loot and you salvage the remains of the bounty hunter's ship.")
             print("Health: -10, Money: +38")
             ship_health -= 10
             money += 38
         elif battle_choice3 == "B":
-            print("You decide that its better to take a portion of the bounty than possibly low value goods. You and the bounty hunter easily overwhelm the smugglers and destroy them. The bounty hunters gives you a portion of the bounty and you salvage the remains of the smugglers ship.")
+            print("You decide that its better to take a portion of the bounty than possibly low value goods. You and the bounty hunter easily overwhelm the smugglers and destroy them. The bounty hunter gives you a portion of the bounty and you salvage the remains of the smugglers ship.")
+            print("Health: -5, Money: +24")
+            ship_health -= 5
+            money += 24
+        elif battle_choice3 == "C":
+            print("You decide its more profitable to take down both ships and claim the rewards for yourself. After engaging in a long gruely battle you come out victorious. Claiming the remains of both ships and collecting the smugglers bounty.")
+            print("Health: -48, Money: +80")
+            ship_health -= 48
+            money += 80
+        else:
+            print(f"{battle_choice3} is an invalid option. Please try again.")
+else:
+    print(f"{event_choice} is an invalid option. Please try again.")
+
+# Run game logic again to have end game results differ
+print(f"Choose from the following options on what you want to do {game_event}: ")
+event_choice = input(f"Your choice {game_event}: ")
+event_choices.append(event_choice)
+print()
+
+# Handle the game logic and branching where each player choice effects the ending
+if event_choice == "Salvage":
+    game_event.remove(event_choice)
+    if has_scanner == True:
+        print("Your scanners picked up no dangerous lifeforms or dangerous ships, leading you to a successful salvage. You gained +25 Money")
+        money += 25
+    elif ship_choice == "A":
+        print("Because you have no scanners you are unaware of any danger. While attempting to salvage a rouge turret fires upon your ship. Ship Health: -10, Money: +15")
+        ship_health -= 10
+        money += 15
+    elif heavy_weapons == True:
+        print("Being unable to scan for danger has you cautious, you fire your cannons to ensure the wreck is safe to salvage, destroying some of the loot in the process. Money: +6")
+        money += 6
+   
+elif event_choice == "Rescue":
+    game_event.remove(event_choice)
+    if has_scanner == True:
+        print("After recieving a distress signal you scan the vessel for danger. Your scanners pick up that the distress signal is coming from a crew of dangerous rouge \nandroids you destroy their vessel and salvage the wreck. Money: +10")
+        money += 10
+    elif ship_choice == "A":
+        print("You've just recieved a distress signal, but you are unaware of any danger that might be present.")
+        print("You must choose your next decision wisely:\nA.) Take no chances and destroy the ship\nB.) Take the chance of saving lives")
+        rescue_choice = input("Your choice (A, B): ")
+        if rescue_choice == "A":
+            print("Unwilling to take any risks you destroy their ship. Unfortunatly those aboard were infact in need of aid. But at least you were able to salvage something. Money: +20")
+            money += 20
+        elif rescue_choice == "B":
+            print("Being the selfless captain you are you take the risk and help those in need. Luckily for you the crew you saved were peaceful. In their gratitude they \noffer you money and one pledges himself to your crew.")
+            print(f'Crew member gained: Rogers, Money: +10')
+            money += 10
+            ship_crew.append("Rogers")
+        else:
+            print(f"'{rescue_choice}' is not a valid option. Please try again.")
+    elif heavy_weapons == True:
+        print("After recieving a message requesting aid you jump to the rescue. Encountering a large vessel poised for attack!")
+        print("After a lengthy and hard fought battle you save the endangered crew but not without casualties. Luckily the crew you saved is  greatful and compensated you the best they could.")
+        print(f"Ship damaged: -30, Crew member lost: {ship_crew[1]}, Money: +25")
+        ship_health -= 30
+        ship_crew.pop(1)
+        money += 25
+elif event_choice == "Battle":
+    game_event.remove(event_choice)
+    if has_scanner:
+        print("While flying across the galaxy you encounter a vessel with a bounty on its head. Unfortunatly they have jammed your scanners leaving you unaware of their power.")
+        print("Will you decide to:\nA.) Engage in battle and collect the bounty\nB.) Do not engage the ship and flee")
+        battle_choice = input("Your choice (A, B): ")
+        if battle_choice == "A":
+            print(f"You decided to ingore the risk and attack the vessel. It was a hard fought battle but the firepower of the enemy vessel was too strong and your ship was destroyed in battle. Health: -{ship_health}")
+            ship_health -= ship_health
+        elif battle_choice == "B":
+            print("You decided to flee rather than engage. A bold move as your scanners picked up that the ship was heavy armored and armed.")
+        else:
+            print(f"{battle_choice} is not a valid option. Please try again.")
+    elif ship_choice == "A":
+        print("While traveling across the galaxy you are attacked by pirates. They've given you two options,  pay them or die.")
+        print("What will you do?: \nA.) Concede, theres no risk getting hurt\nB.) A pirate cannot be trusted, all hands on deck!")
+        battle_choice2 = input("Your choice (A, B): ")
+        if battle_choice2 == "A":
+            print(f"You concede and are willing to pay the price they ask, consequently they have taken a large sum of money and one of your crew members\n Crewmember lost: {ship_crew[0]}, Money: -60")
+            ship_crew.pop(0)
+            money -= 60
+        elif battle_choice2 == "B":
+            print("Unwilling to negotiate with pirate you attack! Lasers stream across the cold vaccum of space until you emerge the victor. You salvage the remains of their vessel and save a prisoner who joins your crew.")
+            print("New Crewmember: Stannis, Money: +15")
+            ship_crew.append("Stannis")
+            money += 15
+        else:
+            print(f"{battle_choice2} is not a valid option. Please try again.")
+    elif heavy_weapons == True:
+        print("You come across a battle between smugglers and a bounty hunter. They both have requested of your help, the smugglers offer you a portion of their goods but the bounty hunter offers you a portion of the bounty reward.")
+        print("What will you do?:\nA.) Side with the smugglers they might have valuable goods.\nB.) Side with the bounty hunter, the reward might be greater than the risks.\nC.) My enemies are many, my equals are none. Attack both and claim the rewards for yourself!")
+        battle_choice3 = input("Your choice (A, B, C): ")
+        if battle_choice3 == "A":
+            print("You decide to team up with the smugglers and you easily take down the bounty hunter. As thanks the smugglers give you a portion of their loot and you salvage the remains of the bounty hunter's ship.")
+            print("Health: -10, Money: +38")
+            ship_health -= 10
+            money += 38
+        elif battle_choice3 == "B":
+            print("You decide that its better to take a portion of the bounty than possibly low value goods. You and the bounty hunter easily overwhelm the smugglers and destroy them. The bounty hunter gives you a portion of the bounty and you salvage the remains of the smugglers ship.")
             print("Health: -5, Money: +24")
             ship_health -= 5
             money += 24
@@ -125,12 +229,12 @@ if ship_health <= 55 and ship_health > 0 and money > 60:
     print("Summary: You sacrificed losing your ship to gain wealth. Luckily it worked out for you.")
 elif ship_health >= 100 and money >= 40:
     print("Summary: You played a safe and cautious game and left with some spoils.")
+elif len(ship_crew) < 3:
+    print("Summary: Your recklessness led to the loss of your crewmembers. I hope it was worth it.")
 elif ship_health >= 80 and money < 40:
     print("Summary: You played it very safe and unfortunatly came up with nothing. Make some risks if you want to make some money.")
 elif len(ship_crew) > 3:
     print("Summary: You worked hard to keep your crew alive and picked up some extra members along the way")
-elif len(ship_crew) < 3:
-    print("Summary: Your recklessness led to the loss of your crewmembers. I hope it was worth it.")
 elif ship_health <= 0:
     print("Summary: You failed as a captian. You let you, your ship, and your crew perish.")
 else:
@@ -139,6 +243,7 @@ else:
 print("=== GAME STATS ===")
 print(f"Ship name: {ship_name}")
 print(f"Ship choice: {ship_choice}")
+print(f"Event Choices: {event_choices}")
 print(f"Ship Health: {ship_health}")
 print(f"Ship Crew: {ship_crew}")
 print(f"Money: {money}")
